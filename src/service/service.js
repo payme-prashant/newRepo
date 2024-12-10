77c0265def19254d69ade80bc7b7d538870c18f7
Prashant Sharma <prashant.sharma@paymeindia.in>
2024-12-10 15:58:29 +0530

    api-change

--- src/service/service.js
+++ src/service/service.js
@@ -100,7 +100,7 @@ export const getPostSignedUrl = (params) => {
 
 
 export const verifySelfie = (params) => {
-  const url = `${API_ENDPOINT}/api/ekyc/selfie/verify/`
+  const url = `${API_ENDPOINT}/api/ekyc/ekyc/selfie_verify/`;
   return axios.post(url, params, {
     headers: headersApplicationJson,
   });
